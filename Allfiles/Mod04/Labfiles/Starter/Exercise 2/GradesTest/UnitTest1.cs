5c13a430aaa748d0b9337cbacbd28c1069206d26
Tecnico - TI <tecnico@epfserver.com.br>
2019-05-22 20:04:28 -0300

    Module 4: Exercise 2

---
+++ Allfiles/Mod04/Labfiles/Starter/Exercise 2/GradesTest/UnitTest1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GradesTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestInitialize]
+        public void Init()
+        {
+            GradesPrototype.Data.DataSource.CreateData();
+        }
+
+        [TestMethod]
+        public void TestValidGrade()
+        {
+            GradesPrototype.Data.Grade grade = new GradesPrototype.Data.Grade(1, "01/01/2012", "Math", "A-", "Very good");
+            Assert.AreEqual(grade.AssessmentDate, "01/01/2012");
+            Assert.AreEqual(grade.SubjectName, "Math");
+            Assert.AreEqual(grade.Assessment, "A-");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestBadDate()
+        {
+            GradesPrototype.Data.Grade grade = new GradesPrototype.Data.Grade(1, "01/01/2023", "Math", "A-", "Very good");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestDateNotRecognized()
+        {
+            GradesPrototype.Data.Grade grade = new GradesPrototype.Data.Grade(1, "01/15/2012", "Math", "A-", "Very good");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestBadAssessment()
+        {
+            GradesPrototype.Data.Grade grade = new GradesPrototype.Data.Grade(1, "01/01/2012", "Math", "F-", "Very good");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestBadSubject()
+        {
+            GradesPrototype.Data.Grade grade = new GradesPrototype.Data.Grade(1, "01/01/2012", "Italian", "A-", "Very good");
+        }
+    }
+}
